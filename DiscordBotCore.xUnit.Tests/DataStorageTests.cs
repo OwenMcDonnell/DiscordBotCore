4d6c167c50f50bf1106648c791dc33836a358fd3
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2019-04-21 10:55:41 +0200

    Test coverage of JsonStorage

--- DiscordBotCore.xUnit.Tests/DataStorageTests.cs
+++ DiscordBotCore.xUnit.Tests/DataStorageTests.cs
@@ -1,6 +1,7 @@
 ﻿using DiscordBotCore.Storage;
 using DiscordBotCore.Storage.Implementations;
 using System;
+using System.IO;
 using Xunit;
 
 namespace DiscordBotCore.xUnit.Tests
@@ -34,8 +35,23 @@ namespace DiscordBotCore.xUnit.Tests
             var actual = storage.RestoreObject<string>(expectedKey);
 
             Assert.Equal(expected, actual);
-
             Assert.Throws<ArgumentException>(() => storage.RestoreObject<object>("FAKE-KEY"));
+        }
+
+        [Fact]
+        public static void JsonStorageTest()
+        {
+            const string expected = "I'm a unit test!";
+            const string expectedKey = "xUnitTests/TEST";
+
+            IDataStorage storage = new JsonStorage();
+
+            storage.StoreObject(expected, expectedKey);
+
+            var actual = storage.RestoreObject<string>(expectedKey);
+
+            Assert.Equal(expected, actual);
+            Assert.Throws<FileNotFoundException>(() => storage.RestoreObject<object>("FAKE-KEY"));
         }
     }
 }
